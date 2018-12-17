fccb3ca48297f9b7b6d9b231dc483700ba1d2de0
Kendall Bates <bates.kendall@gmail.com>
2018-12-17 18:53:52 +0000

    Click to next page, if more comments.

--- main.rb
+++ main.rb
@@ -2,14 +2,22 @@ require "mechanize"
 require "pry"
 
 agent = Mechanize.new
-
 page = agent.get("https://news.ycombinator.com/item?id=18589702")
 
-page.search('.comment').each do |comment|
-  # filter all comments that don't contain 'new york' or 'nyc'. 
-  # Replace with whichever city you like.
-  if comment.text.downcase.include?('new york') || comment.text.downcase.include?('nyc')
-    puts comment.text
-    binding.pry
+# Search for elements with 'comment' class name
+loop do
+  page.search('.comment').each do |comment|
+    # filter all comments that don't contain 'new york' or 'nyc'. 
+    # Replace with whichever city you like.
+    if comment.text.downcase.include?('new york') || comment.text.downcase.include?('nyc')
+      puts comment.text
+    end
+  end
+  
+  # if there are more comments, go to next page
+  if link = page.link_with(:text => 'More')
+    page = link.click
+  else
+    break
   end
 end
