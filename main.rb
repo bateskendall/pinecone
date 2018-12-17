2a54dc3cac4f5f0b0eda375ff3e9bd6dcf34eb21
Kendall Bates <bates.kendall@gmail.com>
2018-12-17 19:25:05 +0000

    Save results to .csv file.

--- main.rb
+++ main.rb
@@ -1,16 +1,23 @@
 require "mechanize"
 require "pry"
+require "csv"
 
 agent = Mechanize.new
 page = agent.get("https://news.ycombinator.com/item?id=18589702")
 
-# Search for elements with 'comment' class name
 loop do
+  # Search for elements with 'comment' class name
   page.search('.comment').each do |comment|
     # filter all comments that don't contain 'new york' or 'nyc'. 
     # Replace with whichever city you like.
     if comment.text.downcase.include?('new york') || comment.text.downcase.include?('nyc')
-      puts comment.text
+      if comment.text.include?(' | ')
+        # split comment text into 'company' and 'description'
+        job = {company: comment.text.split("|").first.strip, description: comment.text.partition('|').last.strip}
+        CSV.open('job_info.csv', 'a+') do |csv|
+          csv << [job[:company], job[:description]]
+        end
+      end
     end
   end
   
